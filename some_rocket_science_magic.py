ea21478dc0955110416fb90d8ac7e98ff6ea9163
d83b38 <d83b38@yandex.ru>
2022-11-21 18:25:41 +0300

    revert bad commit

--- some_rocket_science_magic.py
+++ some_rocket_science_magic.py
@@ -1,1 +1,6 @@
-75 6e 75 6f 78 20 65 71 6e 71 6a 20 74 70 62 6d 67 20 68 6b 66 6d 6b 20 6b 68 6b 77 6a 20 68 68 64 6a 76 20 79 74 68 6c 75 20 71 62 70 74 79 20 65 6d 70 68 75 20 66 73 62 7a 63 20 73 6c 6f 63 6e 20 6b 72 69 61 6f 20 7a 6c 67 6b 6e 20 74 69 73 64 72 20 77 65 69 62 72 20 6b 73 65 76 64 20 66 66 62 63 68 20 67 6b 76 73 77 20 6a 79 75 64 64 20 78 78 79 6a 64 20 64 6e 75 74 67 20 7a 6e 6e 75 69 20 7a 66 6a 75 7a 20 72 71 62 79 64 20 6e 61 75 75 6a 20 66 6f
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(random_state=0)
+X = [[ 1,  2,  3],  # 2 samples, 3 features
+    [11, 12, 13]]
+y = [0, 1]  # classes of each sample
+clf.fit(X, y)
